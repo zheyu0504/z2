824527c0fb68ee37f0b76b99ecfa7b2744f9712a
zheyu0504 <56227631+zheyu0504@users.noreply.github.com>
2019-10-23 09:48:59 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 time=0
 setInterval(auto,4000)
+
 function auto(){
 	time++
 	if(time>2){
 		time=0
 	}
-	$(".show img").fadeOut(400,function(){
+	
+	$(".show img").fadeOut(400, function(){
 		$(".show img").attr("src","img/slide/"+time+".jpg")
 		$(".show img").fadeIn()
 	})
@@ -18,57 +20,52 @@ $(".category-item").hover(function(){
 })
 
 
-
 $("#item1").click(function(){
-	$("#shop1").fadeIn(500)
+	$("#shop1").fadeIn(1000)
 	$("#shop2").fadeOut(1)
 	$("#shop3").fadeOut(1)
 	$("#shop4").fadeOut(1)
 })
+
 
 $("#item2").click(function(){
-	$("#shop2").fadeIn(500)
+	$("#shop2").fadeIn(1000)
 	$("#shop1").fadeOut(1)
 	$("#shop3").fadeOut(1)
 	$("#shop4").fadeOut(1)
 })
+
 
 $("#item3").click(function(){
-	$("#shop3").fadeIn(500)
-	$("#shop1").fadeOut(1)
+	$("#shop3").fadeIn(1000)
 	$("#shop2").fadeOut(1)
+	$("#shop1").fadeOut(1)
 	$("#shop4").fadeOut(1)
 })
 
+
 $("#item4").click(function(){
-	$("#shop4").fadeIn(500)
-	$("#shop1").fadeOut(1)
+	$("#shop4").fadeIn(1000)
 	$("#shop2").fadeOut(1)
 	$("#shop3").fadeOut(1)
+	$("#shop1").fadeOut(1)
 })
 
- t=0
-  $(".btn-shop").click(function(){
-    t++
-  });
+shop=0
 
- $(".btn-shop").click(function(){
-    $("#cart a").text("購物車("+t+")")
-  });
-  
-  $("#cart").click(function(){
-  	$(".login").fadeIn()
-  })
-   $(".login img").click(function(){
-  	$(".login").fadeOut()
-  })
-  
-   $(".btn-login").click(function(){
-   	alert("沒有網路無法登入")
-  	$(".login").fadeOut()
-	
-  })
-  
-   $(".btn").click(function(){
-	alert("資料已送出")
-  })
+$(".btn-shop").click(function(){
+	shop++
+	$("#cart").text("購物車("+shop+")")
+})
+
+
+
+
+
+
+
+
+
+
+
+
